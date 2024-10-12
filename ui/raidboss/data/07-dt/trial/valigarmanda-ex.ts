7de05431ea8eac7b9120c1091a6592e7492ea71e
Jacob Keller <jacob.keller@gmail.com>
2024-10-11 23:36:25 -0700

    personal: call tower soak for tank based on who got hit
    Currently, a single callout of 30 seconds is put up to warn tanks to
    soak/swap the tower.
    
    Instead, keep track of which tank got hit, and call out soak or swap
    based on who got hit by the tank tower. This enables us to avoid calling
    the direction for a tank who just needs to stand in the tower (reducing
    noise) and use an alarm vs an alert so tanks can keep track of the beat
    of the fight and not forget to swap in to the tower.
    
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- ui/raidboss/data/07-dt/trial/valigarmanda-ex.ts
+++ ui/raidboss/data/07-dt/trial/valigarmanda-ex.ts
@@ -12,6 +12,7 @@ export interface Data extends RaidbossData {
   arcaneLaneSafe: ArcaneLane[];
   avalancheSafe?: 'frontRight' | 'backLeft';
   iceSphereAttackCount: number;
+  mountainFireSoak: boolean | undefined;
 }
 
 // Vali uses uncasted abilities to move between left, middle, and right.
@@ -108,6 +109,7 @@ const triggerSet: TriggerSet<Data> = {
       arcaneLaneSafe: [...arcaneLanesConst],
       phase: 'start',
       iceSphereAttackCount: 0,
+      mountainFireSoak: undefined,
     };
   },
   triggers: [
@@ -226,11 +228,9 @@ const triggerSet: TriggerSet<Data> = {
       type: 'Ability',
       netRegex: { id: '900C', source: 'Valigarmanda', capture: false },
       condition: (data) => data.role === 'tank',
-      // There's ~5.5s between the end of the cast and damage applied from first tower soak.
-      // The tower soak/damage happens six times; use a long duration to keep this reminder up.
-      durationSeconds: 30.5,
-      // use infoText to distinguish from wedge direction alert calls at the same time
+      durationSeconds: 6,
       infoText: (_data, _matches, output) => output.soakSwap!(),
+      run: (data) => data.mountainFireSoak = undefined,
       outputStrings: {
         soakSwap: {
           en: 'Tank Tower (soak/swap)',
@@ -242,6 +242,37 @@ const triggerSet: TriggerSet<Data> = {
           tc: '雙坦輪換踩塔',
         },
       },
+    },
+    {
+      id: 'Valigarmanda Ex Mountain Fire Tank Swap',
+      type: 'StartsUsing',
+      netRegex: { id: '9019', source: 'Valigarmanda', capture: false },
+      condition: (data) => data.role === 'tank' && data.mountainFireSoak === true,
+      alarmText: (_data, _matches, output) => output.soakSwap!(),
+      outputStrings: {
+        soakSwap: {
+          en: 'Tank Tower (soak/swap)',
+          de: 'Tank Türme (nehmen/wechseln)',
+          fr: 'Tank : Tour (encaisser/swap)',
+          ja: 'タンク塔 (踏む/スイッチ)',
+          cn: '双T轮换踩塔',
+          ko: '탱커 기둥 (밟기/교대)',
+        },
+      },
+    },
+    {
+      id: 'Valigarmanda Ex Mountain Fire Soak Setup',
+      type: 'Ability',
+      netRegex: { id: '9019', source: 'Valigarmanda' },
+      condition: Conditions.targetIsYou(),
+      run: (data) => data.mountainFireSoak = false,
+    },
+    {
+      id: 'Valigarmanda Ex Mountain Fire Vuln',
+      type: 'Ability',
+      netRegex: { id: '9019', source: 'Valigarmanda' },
+      condition: Conditions.targetIsNotYou(),
+      run: (data) => data.mountainFireSoak = true,
     },
     {
       id: 'Valigarmanda Ex Mountain Fire First Wedge',
@@ -258,6 +289,7 @@ const triggerSet: TriggerSet<Data> = {
       id: 'Valigarmanda Ex Mountain Fire Subsequent Wedge',
       type: 'Ability',
       netRegex: { id: mtFireIds, source: 'Valigarmanda' },
+      condition: (data) => data.role !== 'tank' || data.mountainFireSoak === false,
       alertText: (_data, matches, output) => {
         const safe = mtFireIdToSafeMap[matches.id];
         if (safe === undefined)
