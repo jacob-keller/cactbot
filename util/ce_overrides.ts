7c2a4d7483306a2b87e3966bdada266549c017fc
Jacob Keller <jacob.keller@gmail.com>
2025-06-12 16:39:47 -0700

    util: generate CE info from xivapi
    In preparation for adding proper CE support to the encounter tools,
    first start generating the encounter data we need. Unfortunately it is
    currently unknown how the game generates the DirectorUpdate IDs used in
    the network log messages, but we can at least pull the translated names
    of the encounters from xivapi.
    
    Add a new gen_ce_info_and_maps.ts generator script which will generate
    several files including:
    
     * resources/bozja_encounters.ts
     * resources/bozja_ce_map.ts
     * resources/zadnor_encounters.ts
     * resources/zadnor_ce_map.ts
     * resources/south_horn_encounters.ts
     * resources/south_horn_ce_map.ts
    
    I opted to split by zone since that will be useful both to limit
    including only the relevent CEs in a given trigger file, and verify that
    the zone is correct when we see a relevant network log message in the
    encounter tools. This does lead to a lot of extra files. I wasn't sure
    how to combine the file results given the way xivapi.writeFile() works.
    
    The list of DirectorUpdate IDs is maintained as part of ce_overrides.ts,
    which I tried to structure in a similar way as the zone_overrides.ts.
    Hopefully we can re-use that style if we need to override any data from
    the API in the future.
    
    I have not made any attempt to support pulling the other translations
    for the Chinese and Korean languages yet. I also only pulled the names,
    since none of the other data was very relevant for the encounter tools.
    
    The Forked Tower of Blood is listed in the API, but it is really made of
    multiple encounters each with their own name and director ID. To handle
    these, synthetic data for these encounters is maintained within the
    ce_overrides.ts file. This file could also eventually contain
    translation data for languages which aren't part of the API.
    
    The generated CE Maps ordering is a bit wonky. This appears to happen
    because the default sort ordering treats numeric values as coming first
    prior to non-numeric strings, so all the hex-digit strings come
    afterwards. I tried to fix this by adjusting the sort function, but this
    produces undesirable changes in many other generated files.
    
    Suggested-by: Valarnin <valarnin@users.noreply.github.com>
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

---
+++ util/ce_overrides.ts
@@ -0,0 +1,144 @@
+// This file contains data used in the `gen_ce_info_and_maps.ts` file to
+// supplement the data from xivapi. In the future it may also contain proper
+// override data in the event that something is removed from the game.
+//
+// Update this file manually to maintain this data; then re-run the script.
+
+import { LocaleText } from '../types/trigger';
+
+type NameKeyToDirectorUpdateId = {
+  [name: string]: string;
+};
+
+type CeInfoType = {
+  [key: string]: {
+    directorId: string;
+    name: LocaleText;
+  };
+};
+
+type ZoneNameToCeInfo = {
+  [name: string]: CeInfoType;
+};
+
+type OverrideContainer = {
+  directorIds: NameKeyToDirectorUpdateId;
+  syntheticCEs: ZoneNameToCeInfo;
+};
+
+// List of values appearing in the DirectorUpdate network log events.
+//
+// Currently, these ids are unfortunately gathered by hand and don't seem to
+// correlate to any particular bits of data within xivapi, or other sources.
+// There is a game log message with the CE name when you register for a CE,
+// as well as a 0x21 DirectorUpdate message with this id when you actually
+// teleport into the event. The 0x21 message is used by encounter tools and
+// by timelines and triggers to identify the critical engagement or
+// encounter.
+const _DIRECTOR_UPDATE_VALUES: NameKeyToDirectorUpdateId = {
+  // The Bozjan Southern Front
+  'TheBattleOfCastrumLacusLitore': '1D7',
+  'KillItWithFire': '1D4',
+  'TheBayingOfTheHounds': '1CC',
+  'VigilForTheLost': '1D0',
+  'AcesHigh': '1D2',
+  'TheShadowOfDeathsHand': '1CD',
+  'TheFinalFurlong': '1D5',
+  'TheHuntForRedChoctober': '1CA',
+  'BeastOfMan': '1DB',
+  'TheFiresOfWar': '1D6',
+  'PatriotGames': '1D1',
+  'TrampledUnderHoof': '1CE',
+  'AndTheFlamesWentHigher': '1D3',
+  'MetalFoxChaos': '1CB',
+  'RiseOfTheRobots': '1DF',
+  'WhereStrodeTheBehemoth': '1DC',
+  // Zadnor
+  'TheDalriada': '213',
+  'OnSerpentsWings': '211',
+  'FeelingTheBurn': '20E',
+  'TheBrokenBlade': '21F',
+  'FromBeyondTheGrave': '21B',
+  'WithDiremiteAndMain': '221',
+  'HereComesTheCavalry': '21C',
+  'HeadOfTheSnake': '21E',
+  'ThereWouldBeBlood': '210',
+  'NeverCryWolf': '20F',
+  'TimeToBurn': '21D',
+  'LeanMeanMagitekMachines': '218',
+  'WornToAShadow': '222',
+  'AFamiliarFace': '212',
+  'LooksToDieFor': '207',
+  'TakingTheLyonsShare': '220',
+  // Occult Crescent: South Horn
+  'ScourgeOfTheMind': '320',
+  'TheBlackRegiment': '322',
+  'TheUnbridled': '348',
+  'CrawlingDeath': '330',
+  'CalamityBound': '32F',
+  'TrialByClaw': '349',
+  'FromTimesBygone': '323',
+  'CompanyOfStone': '343',
+  'SharkAttack': '32E',
+  'OnTheHunt': '338',
+  'WithExtremePrejudice': '339',
+  'NoiseComplaint': '327',
+  'CursedConcern': '32B',
+  'EternalWatch': '329',
+  'FlameOfDusk': '32A',
+};
+
+// Forked Tower CEs do not get listed in the XIV API.
+const _SYNTHETIC_CE_INFO: ZoneNameToCeInfo = {
+  'South Horn': {
+    'DemonTablet': {
+      'directorId': '33B',
+      'name': {
+        'en': 'Demon Tablet',
+      },
+    },
+    'CentralGallery': {
+      'directorId': '33F',
+      'name': {
+        'en': 'Central Passages',
+      },
+    },
+    'DeadStars': {
+      'directorId': '33C',
+      'name': {
+        'en': 'Dead Stars: Nereid, Triton, and Phobos',
+      },
+    },
+    'UpperExterior': {
+      'directorId': '340',
+      'name': {
+        'en': 'Pronged Passages',
+      },
+    },
+    'MarbleDragon': {
+      'directorId': '33D',
+      'name': {
+        'en': 'Marble Dragon',
+      },
+    },
+    'BindingLock': {
+      'directorId': '341',
+      'name': {
+        'en': 'The Binding Lock',
+      },
+    },
+    'InfamyOfBloodMagitaur': {
+      'directorId': '33E',
+      'name': {
+        'en': 'Infamy Of Blood: Magitaur',
+      },
+    },
+  },
+};
+
+const Overrides: OverrideContainer = {
+  directorIds: _DIRECTOR_UPDATE_VALUES,
+  syntheticCEs: _SYNTHETIC_CE_INFO,
+};
+
+export default Overrides;
